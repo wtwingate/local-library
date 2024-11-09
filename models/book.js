310945163ad6ba8381d12ca8f6f8f7e8b0f0842f
William Wingate <wtwingate@gmail.com>
2024-11-09 16:20:53 -0500

    Write models for database

---
+++ models/book.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const BookSchema = new Schema({
+  title: { type: String, required: true },
+  author: { type: Schema.Types.ObjectId, ref: "Author", required: true },
+  summary: { type: String, required: true },
+  isbn: { type: String, required: true },
+  genre: [{ type: Schema.Type.ObjectId, ref: "Genre" }],
+});
+
+// Virtual for Book's URL
+BookSchema.virtual("url").get(function () {
+  return `/catalog/book/${this._id}`;
+});
+
+// Export model
+module.exports = mongoose.model("Book", BookSchema);
