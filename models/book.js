82e25582d78de369e751ab4628dc67bb5780bb2c
William Wingate <wtwingate@gmail.com>
2024-11-09 16:34:38 -0500

    Add test data to MongoDB

--- models/book.js
+++ models/book.js
@@ -7,7 +7,7 @@ const BookSchema = new Schema({
   author: { type: Schema.Types.ObjectId, ref: "Author", required: true },
   summary: { type: String, required: true },
   isbn: { type: String, required: true },
-  genre: [{ type: Schema.Type.ObjectId, ref: "Genre" }],
+  genre: [{ type: Schema.Types.ObjectId, ref: "Genre" }],
 });
 
 // Virtual for Book's URL
