554c4fab4b0a4e99d21fb8a66c26a30c2d55ae25
William Wingate <wtwingate@gmail.com>
2024-11-11 11:28:08 -0500

    Create homepage for Local Library website

--- populatedb.js
+++ populatedb.js
@@ -1,7 +1,7 @@
 #! /usr/bin/env node
 
 console.log(
-  'This script populates some test books, authors, genres and bookinstances to your database. Specified database as argument - e.g.: node populatedb "mongodb+srv://cooluser:coolpassword@cluster0.cojoign.mongodb.net/local_library?retryWrites=true&w=majority&appName=Cluster0"'
+  'This script populates some test books, authors, genres and bookinstances to your database. Specified database as argument - e.g.: node populatedb "mongodb+srv://cooluser:coolpassword@cluster0.cojoign.mongodb.net/local_library?retryWrites=true&w=majority&appName=Cluster0"',
 );
 
 // Get arguments passed on command line
@@ -116,7 +116,7 @@ async function createBooks() {
       "I have stolen princesses back from sleeping barrow kings. I burned down the town of Trebon. I have spent the night with Felurian and left with both my sanity and my life. I was expelled from the University at a younger age than most people are allowed in. I tread paths by moonlight that others fear to speak of during day. I have talked to Gods, loved women, and written songs that make the minstrels weep.",
       "9781473211896",
       authors[0],
-      [genres[0]]
+      [genres[0]],
     ),
     bookCreate(
       1,
@@ -124,7 +124,7 @@ async function createBooks() {
       "Picking up the tale of Kvothe Kingkiller once again, we follow him into exile, into political intrigue, courtship, adventure, love and magic... and further along the path that has turned Kvothe, the mightiest magician of his age, a legend in his own time, into Kote, the unassuming pub landlord.",
       "9788401352836",
       authors[0],
-      [genres[0]]
+      [genres[0]],
     ),
     bookCreate(
       2,
@@ -132,7 +132,7 @@ async function createBooks() {
       "Deep below the University, there is a dark place. Few people know of it: a broken web of ancient passageways and abandoned rooms. A young woman lives there, tucked among the sprawling tunnels of the Underthing, snug in the heart of this forgotten place.",
       "9780756411336",
       authors[0],
-      [genres[0]]
+      [genres[0]],
     ),
     bookCreate(
       3,
@@ -140,7 +140,7 @@ async function createBooks() {
       "Humankind headed out to the stars not for conquest, nor exploration, nor even for curiosity. Humans went to the stars in a desperate crusade to save intelligent life wherever they found it. A wave of death is spreading through the Milky Way galaxy, an expanding sphere of lethal gamma ...",
       "9780765379528",
       authors[1],
-      [genres[1]]
+      [genres[1]],
     ),
     bookCreate(
       4,
@@ -148,7 +148,7 @@ async function createBooks() {
       "In Ben Bova's previous novel New Earth, Jordan Kell led the first human mission beyond the solar system. They discovered the ruins of an ancient alien civilization. But one alien AI survived, and it revealed to Jordan Kell that an explosion in the black hole at the heart of the Milky Way galaxy has created a wave of deadly radiation, expanding out from the core toward Earth. Unless the human race acts to save itself, all life on Earth will be wiped out...",
       "9780765379504",
       authors[1],
-      [genres[1]]
+      [genres[1]],
     ),
     bookCreate(
       5,
@@ -156,7 +156,7 @@ async function createBooks() {
       "Summary of test book 1",
       "ISBN111111",
       authors[4],
-      [genres[0], genres[1]]
+      [genres[0], genres[1]],
     ),
     bookCreate(
       6,
@@ -164,7 +164,7 @@ async function createBooks() {
       "Summary of test book 2",
       "ISBN222222",
       authors[4],
-      false
+      false,
     ),
   ]);
 }
@@ -177,7 +177,7 @@ async function createBookInstances() {
       books[0],
       "London Gollancz, 2014.",
       false,
-      "Available"
+      "Available",
     ),
     bookInstanceCreate(1, books[1], " Gollancz, 2011.", false, "Loaned"),
     bookInstanceCreate(2, books[2], " Gollancz, 2015.", false, false),
@@ -186,42 +186,42 @@ async function createBookInstances() {
       books[3],
       "New York Tom Doherty Associates, 2016.",
       false,
-      "Available"
+      "Available",
     ),
     bookInstanceCreate(
       4,
       books[3],
       "New York Tom Doherty Associates, 2016.",
       false,
-      "Available"
+      "Available",
     ),
     bookInstanceCreate(
       5,
       books[3],
       "New York Tom Doherty Associates, 2016.",
       false,
-      "Available"
+      "Available",
     ),
     bookInstanceCreate(
       6,
       books[4],
       "New York, NY Tom Doherty Associates, LLC, 2015.",
       false,
-      "Available"
+      "Available",
     ),
     bookInstanceCreate(
       7,
       books[4],
       "New York, NY Tom Doherty Associates, LLC, 2015.",
       false,
-      "Maintenance"
+      "Maintenance",
     ),
     bookInstanceCreate(
       8,
       books[4],
       "New York, NY Tom Doherty Associates, LLC, 2015.",
       false,
-      "Loaned"
+      "Loaned",
     ),
     bookInstanceCreate(9, books[0], "Imprint XXX2", false, false),
     bookInstanceCreate(10, books[1], "Imprint XXX3", false, false),
